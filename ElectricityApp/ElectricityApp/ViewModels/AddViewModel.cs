910b772e9886917eba4c4f4748da9496746eb1e4
Vitalii Voitovych <vitalikvv2119@gmail.com>
2024-06-29 15:22:33 +0300

    Reworked NumberBox. Date selection has been changed.

--- ElectricityApp/ElectricityApp/ViewModels/AddViewModel.cs
+++ ElectricityApp/ElectricityApp/ViewModels/AddViewModel.cs
@@ -7,26 +7,37 @@ namespace ElectricityApp.ViewModels;
 
 public partial class AddViewModel(NotesService _notesService) : ObservableObject
 {
-    [ObservableProperty] private DateOnly _date = DateOnly.FromDateTime(DateTime.Now);
+    private static readonly Dictionary<string, int> _months = new()
+    {
+        { "Січень", 1}, { "Лютий" , 2}, { "Березень" , 3}, { "Квітень" , 4},
+        { "Травень", 5}, { "Червень" , 6}, { "Липень" , 7}, { "Серпень" , 8},
+        { "Вересень", 9}, { "Жовтень" , 10}, { "Листопад" , 11}, { "Грудень" , 12},
+    };
+
+    public List<string> Months => [.. _months.Keys];
+
+    public List<int> Years => [.. Enumerable.Range(DateTime.Now.Year - 5, 20)];
+
+    [ObservableProperty] private string _selectedMonth = _months.First(m => m.Value == DateTime.Now.Month).Key;
+    [ObservableProperty] private int _selectedYear = DateTime.Now.Year;
     [ObservableProperty] private int _dayKilowattConsumed;
     [ObservableProperty] private int _nightKilowattConsumed;
     [ObservableProperty] private decimal _kilowattPerHourPrice = 4.32m;
 
     [RelayCommand]
-    private void Add()
+    private async Task Add()
     {
         var amountToPlay = 
             DayKilowattConsumed * KilowattPerHourPrice + NightKilowattConsumed * (KilowattPerHourPrice * 0.5m);
 
-        var record = new ElectricityConsumption(Date, DayKilowattConsumed, NightKilowattConsumed, amountToPlay);
+        var record = new ElectricityConsumption(new DateOnly(SelectedYear, _months[SelectedMonth], 1), DayKilowattConsumed, NightKilowattConsumed, amountToPlay);
         try
         {
-            _notesService.AddNote(record);
-            Date = Date.AddMonths(1);
+            await _notesService.AddNote(record);
         }
         catch (ArgumentException ex)
         {
-            Shell.Current.DisplayAlert("Помилка!", ex.Message, "Зрозуміло");
+            await Shell.Current.DisplayAlert("Помилка!", ex.Message, "Зрозуміло");
         }
     }
 }
