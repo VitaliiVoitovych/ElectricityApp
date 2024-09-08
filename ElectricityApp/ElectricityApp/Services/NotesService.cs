587a4b61e0c9e7fda175a0d11d0e02e185602765
Vitalii Voitovych <vitalikvv2119@gmail.com>
2024-09-08 16:50:15 +0300

    Small changes

--- ElectricityApp/ElectricityApp/Services/NotesService.cs
+++ ElectricityApp/ElectricityApp/Services/NotesService.cs
@@ -1,5 +1,4 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using ElectricityApp.EfStructures;
+﻿using ElectricityApp.EfStructures;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElectricityApp.Services;
