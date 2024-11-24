a0e41a44d2b2df9bb2b9aabdd93e4ce730fcd170
Vitalii Voitovych <vitalii.voitovych@protonmail.com>
2024-11-24 23:12:55 +0200

    Rework stepper(use native controls)

--- ElectricityApp/ElectricityApp.Droid/MauiProgram.cs
+++ ElectricityApp/ElectricityApp.Droid/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Maui.Handlers;
+﻿using ElectricityApp.Controls;
+using ElectricityApp.Droid.Handlers;
+using Microsoft.Maui.Handlers;
 
 namespace ElectricityApp.Droid;
 
@@ -11,6 +13,10 @@ public static class MauiProgram
         AddControlsHandlers();
 
         builder
+            .ConfigureMauiHandlers(handlers =>
+            {
+                handlers.AddHandler<StepperWithInput, StepperWithInputHandler>();
+            })
             .UseSharedMauiApp();
 
         return builder.Build();
@@ -26,33 +32,6 @@ public static class MauiProgram
         PickerHandler.Mapper.AppendToMapping(nameof(Picker), (handler, view) =>
         {
             handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.ParseColor("#abb0b3"));
-        });
-
-        StepperHandler.Mapper.AppendToMapping(nameof(Stepper), (handler, view) =>
-        {
-            var buttonBackgroundColor = Android.Graphics.Color.ParseColor("#1d3039");
-            var enabledTextColor = Android.Graphics.Color.ParseColor("#abb0b3");
-            var disabledTextColor = Android.Graphics.Color.ParseColor("#696d70");
-
-            var states = new int[][]
-            {
-                [Android.Resource.Attribute.StateEnabled],
-                [-Android.Resource.Attribute.StateEnabled],
-            };
-
-            int[] colors = [enabledTextColor, disabledTextColor];
-
-            var textColorList = new Android.Content.Res.ColorStateList(states, colors);
-
-            var linearLayout = handler.PlatformView as Android.Widget.LinearLayout;
-            var button1 = linearLayout.GetChildAt(0) as Android.Widget.Button;
-            var button2 = linearLayout.GetChildAt(1) as Android.Widget.Button;
-
-            button1!.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(buttonBackgroundColor);
-            button2!.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(buttonBackgroundColor);
-
-            button1!.SetTextColor(textColorList);
-            button2!.SetTextColor(textColorList);
         });
     }
 }
